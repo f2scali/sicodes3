ca19f8f1d01bf29b45164d1c0be692aff4862e8f
issblann <anaisabelpp@outlook.com>
2024-12-11 13:56:20 -0500

    agregamos cors config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { SucursalModule } from './modules/sucursal.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(SucursalModule), { fallbackOnErrors: true });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
